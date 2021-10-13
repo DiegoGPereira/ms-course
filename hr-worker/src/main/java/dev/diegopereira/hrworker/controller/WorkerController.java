f7c1cedc9eb2707b7354841fdaaa558bc9ded2da
Diego Guedes Pereira <diegoguedespereira@gmail.com>
2021-10-12 23:51:41 -0300

    RestTemplate

--- hr-worker/src/main/java/dev/diegopereira/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/dev/diegopereira/hrworker/controller/WorkerController.java
@@ -7,11 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.diegopereira.hrworker.domain.Worker;
-import dev.diegopereira.hrworker.repository.WorkerRepository;
 import dev.diegopereira.hrworker.service.WorkerService;
 
 @RestController
